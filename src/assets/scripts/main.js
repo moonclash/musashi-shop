f04c82b761fb7d45588cb4ef889852763f6c585e
Emil Mladenov <emladenoff@gmail.com>
2016-11-24 23:10:36 +0000

    Added homepage design, initiating responsive design phase

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -1,6 +1,7 @@
 $(document).ready(function(){
 
   var servicesBackground = $('.services').css('background-image');
+  var samuraiParallax = 900;
   $('.service').each(function(){
     $(this).on('mouseenter',function(){
       $('.services').css('background-image',$(this).css('background-image'));
@@ -8,5 +9,11 @@ $(document).ready(function(){
     $(this).mouseleave(function(){
        $('.services').css('background-image',servicesBackground);
     });
+  });
+
+  $('.philosophy').on('mousemove',function(){
+      samuraiParallax -= 0.3;
+    $(this).css('background-position',samuraiParallax + 'px,center');
+   console.log(samuraiParallax);
   });
 });
