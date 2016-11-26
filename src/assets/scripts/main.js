8f0ec57c063f4d8ce11b7533db217972697f9bd1
Emil Mladenov <emladenoff@gmail.com>
2016-11-26 01:01:27 +0000

    design kinda complete

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -2,6 +2,8 @@ $(document).ready(function(){
 
   var servicesBackground = $('.services').css('background-image');
   var samuraiParallax = 900;
+  var phoneRegex = /^(\+44\s?7\d{3}|\(?07\d{3}\)?)\s?\d{3}\s?\d{3}$/gi;
+  var formFlag = false;
   $('.service').each(function(){
     $(this).on('mouseenter',function(){
       $('.services').css('background-image',$(this).css('background-image'));
@@ -17,7 +19,18 @@ $(document).ready(function(){
    console.log(samuraiParallax);
   });
 
-  $('.nav-toggle').on('click',function(){
+  $('.nav-toggle').on('click',function(e){
+    e.preventDefault();
     $('nav').toggleClass('toggle-nav');
+  });
+
+  $('form button').on('click',function(e) {
+    e.preventDefault();
+    var phoneValue = $('input[type="number"]').val(); 
+    if (phoneValue.match(phoneRegex) && $('input').val().length > 0) {
+      $('form').submit().animate({opacity: 0});
+    }else {
+      $('form').toggleClass('form-shake');
+    }
   })
 });
